ef95679a28e05cb006223b4bc5d5cd1bbe156cee
Jonathan Robertson <jrob356@icloud.com>
2022-04-05 09:19:26 -0500

    register papertrail for logging on GameStartDone

--- PapertrailFor7DTD/API.cs
+++ PapertrailFor7DTD/API.cs
@@ -4,11 +4,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace PapertrailFor7DTD {
     internal class API : IModApi {
         public void InitMod(Mod _modInstance) {
             ModEvents.GameAwake.RegisterHandler(PapertrailLogger.Initialize);
+            ModEvents.GameStartDone.RegisterHandler(() => Log.LogCallbacks += LogToPapertrail);
+        }
+
+        private static void LogToPapertrail(string _msg, string _trace, LogType _type) {
+            switch (_type) {
+                case LogType.Error:
+                case LogType.Exception:
+                    PapertrailLogger.Log(Severity.Error, string.IsNullOrEmpty(_trace) ? _msg : $"{_msg}\n{_trace}");
+                    break;
+                case LogType.Log:
+                    PapertrailLogger.Log(Severity.Informational, string.IsNullOrEmpty(_trace) ? _msg : $"{_msg}\n{_trace}");
+                    break;
+                case LogType.Warning:
+                    PapertrailLogger.Log(Severity.Warning, string.IsNullOrEmpty(_trace) ? _msg : $"{_msg}\n{_trace}");
+                    break;
+            }
         }
     }
 }
