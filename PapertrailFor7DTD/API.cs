371fb58e4d2a55f422ef89de2211ea881bf6cff9
Jonathan Robertson <jrob356@icloud.com>
2022-04-05 09:12:27 -0500

    add 7dtd server references

---
+++ PapertrailFor7DTD/API.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PapertrailFor7DTD {
+    internal class API : IModApi {
+        public void InitMod(Mod _modInstance) {
+
+        }
+    }
+}
---
+++ PapertrailFor7DTD/SDK/Severity.cs
@@ -0,0 +1,13 @@
+﻿namespace PapertrailFor7DTD.SDK {
+    public enum Severity {
+        Off = -1,
+        Emergency = 0,
+        Alert = 1,
+        Critical = 2,
+        Error = 3,
+        Warning = 4,
+        Notice = 5,
+        Informational = 6,
+        Debug = 7,
+    }
+}
